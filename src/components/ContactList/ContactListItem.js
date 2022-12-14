e297ab79541eb8e962e9b43b70a871f5c76b69e6
Matyash-Oleksander <matyash19831010@gmail.com>
2022-12-14 14:34:14 +0200

    ДЗ готово

---
+++ src/components/ContactList/ContactListItem.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { deleteItem } from 'redux/itemsSlice';
+import css from './ContactList.module.css';
+
+export const ContactListItem = ({ id, name, number }) => {
+  const dispatch = useDispatch();
+  const handleDelete = () => dispatch(deleteItem(id));
+
+  return (
+    <li key={id}>
+      <p className={css.listItem}>
+        {name} - {number}
+      </p>
+      <button className={css.buttonList} type="button" onClick={handleDelete}>
+        Delete
+      </button>
+    </li>
+  );
+};
+
+ContactListItem.propType = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  number: PropTypes.string.isRequired,
+};
